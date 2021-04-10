ac5118f4a6e476614b40c481c635a07daf8cd258
Sam Ayler <sayler8701@gmail.com>
2021-04-09 17:58:54 -0700

    adding chat functionality in window tested and working

--- client/src/Components/ChatWindow.js
+++ client/src/Components/ChatWindow.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://localhost:5000";
 
-const ChatWindow = () => {
-	useEffect(() => {
-		const socket = socketIOClient(ENDPOINT, {
-			transports: ["websocket"],
-		});
+const socket = socketIOClient(ENDPOINT, {
+	transports: ["websocket"],
+});
 
+const ChatWindow = () => {
+	const [message, setmessage] = useState(null);
+	const [newMessage, setnewMessage] = useState([]);
+
+	const onChange = (e) => {
+		setmessage({ ...message, [e.target.name]: e.target.value });
+
+		message &&
+			socket.on("typing", () => {
+				socket.emit("typing", message.name);
+			});
+	};
+
+	useEffect(() => {
 		socket.on("connection", () => {
 			console.log(socket.id);
 		});
@@ -17,16 +28,52 @@ const ChatWindow = () => {
 			console.log("user has disconnected");
 		});
 	}, []);
+
+	useEffect(() => {
+		socket.on("chat", (data) => {
+			setnewMessage((arr) => [...arr, data]);
+		});
+	}, []);
+
+	const sendMessage = (e) => {
+		e.preventDefault();
+
+		socket.emit("chat", {
+			message: message.message,
+			handle: message.handle,
+		});
+	};
+
 	return (
 		<>
 			<div id="mario-chat">
 				<div id="chat-window">
-					<div id="output"></div>
+					<div id="output">
+						{newMessage.map((chat, index) => (
+							<p key={index}>
+								<strong>{chat.handle}:</strong> {chat.message}
+							</p>
+						))}
+					</div>
 					<div id="feedback"></div>
 				</div>
-				<input id="handle" type="text" placeholder="Handle" />
-				<input id="message" type="text" placeholder="Message" />
-				<button id="send">Send</button>
+				<input
+					onChange={onChange}
+					id="handle"
+					name="handle"
+					type="text"
+					placeholder="Handle"
+				/>
+				<input
+					onChange={onChange}
+					id="message"
+					name="message"
+					type="text"
+					placeholder="Message"
+				/>
+				<button onClick={sendMessage} id="send">
+					Send
+				</button>
 			</div>
 		</>
 	);
--- server.js
+++ server.js
@@ -20,6 +20,14 @@ const io = socket(server);
 io.on("connection", (socket) => {
 	console.log("socket connection made from server", socket.id);
 
+	socket.on("chat", (data) => {
+		io.sockets.emit("chat", data);
+	});
+
+	socket.on("typing", (data) => {
+		socket.broadcast.emit("typing", data);
+	});
+
 	socket.on("disconnect", () => {
 		console.log("user disconnected");
 	});
